02e867bd32bc484e3b0ca9cf0cba3f7ccc90a155
pyto86 <pyto86.me@gmail.com>
2022-04-23 23:33:00 +0900

    refactor aka4 client

--- src/lib/aka4.rb
+++ src/lib/aka4.rb
@@ -7,30 +7,40 @@ require 'json'
 BASE_URL = 'https://atnd.ak4.jp/api/cooperation'
 CORPORATION_ID = ENV['CORPORATION_ID']
 
-START_WORK = 11
-FINISH_WORK = 12
+TYPE_MAP = { 11 => '出勤', 12 => '退勤' }.freeze
 
 # Aka4
 class Aka4Client
   def reissue_token(token:)
-    uri = URI.parse("#{BASE_URL}/token/reissue/#{CORPORATION_ID}")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    req = Net::HTTP::Post.new(uri.request_uri, { 'Content-Type': 'application/json' })
-    req.body = { token: token }.to_json
-    resp = http.request(req)
-    json = JSON.parse(resp.body)
-    json['response']['token'] if json['success']
+    resp = post_to_aka4("/token/reissue/#{CORPORATION_ID}", { token: token })
+    resp['token']
   end
 
   def punch(token:)
-    uri = URI.parse("#{BASE_URL}/#{CORPORATION_ID}/stamps")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    req = Net::HTTP::Post.new(uri.request_uri, { 'Content-Type': 'application/json' })
-    req.body = { token: token }.to_json
-    resp = http.request(req)
-    json = JSON.parse(resp.body)
-    json['success']
+    resp = post_to_aka4("/#{CORPORATION_ID}/stamps", { token: token })
+    [TYPE_MAP[resp['type']], resp['stampedAt']]
   end
+end
+
+def post_to_aka4(path, data)
+  resp = post_json("#{BASE_URL}#{path}", data)
+  raise StandardError, resp.body unless resp.code.to_i == 200
+
+  json = JSON.parse(resp.body)
+  unless json['success']
+    raise StandardError, json['errors'].map do |error|
+      "#{error.code}: #{error.message}"
+    end.join("\n")
+  end
+
+  json['response']
+end
+
+def post_json(url, data)
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  req = Net::HTTP::Post.new(uri.request_uri, { 'Content-Type': 'application/json' })
+  req.body = data.to_json
+  http.request(req)
 end
